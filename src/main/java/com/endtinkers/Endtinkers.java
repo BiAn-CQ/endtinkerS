ff35b09fd6a5ac46dfbcf260c9ea94b46c2cf276
bian <3103817026@qq.com>
2025-04-06 22:04:26 +0800

    999

--- src/main/java/com/endtinkers/Endtinkers.java
+++ src/main/java/com/endtinkers/Endtinkers.java
@@ -1,8 +1,12 @@
 package com.endtinkers;
 
+import com.endtinkers.modifier.register.ModifierRegister;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -10,11 +14,10 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,13 +31,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
-
+import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegister;
+import slimeknights.tconstruct.library.modifiers.util.StaticModifier;
 
 @Mod(Endtinkers.MODID)
 public class Endtinkers {
 
 
-    public static final String MODID = "endtinhers";
+    public static final String MODID = "endtinkers";
 
     private static final Logger LOGGER = LogUtils.getLogger();
 
@@ -42,15 +46,30 @@ public class Endtinkers {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
 
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
 
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
+    public static final RegistryObject<Block> dot_block = BLOCKS.register("dot_block", () -> new Block(BlockBehaviour.Properties.of().strength(4.0f).sound(SoundType.COPPER)));
+    public static final RegistryObject<Item> DOT_BLOCK_ITEM = ITEMS.register("dot_block", () -> new BlockItem(dot_block.get(), new Item.Properties()));
+    public static final RegistryObject<Item> dot_item = ITEMS.register("dot_item", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> dot_apple = ITEMS.register("dot_apple", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().nutrition(20).saturationMod(20f).effect(new MobEffectInstance(MobEffects.REGENERATION,600*20,4),1f).build())));
+
+    public static ModifierDeferredRegister MODIFIERS = ModifierDeferredRegister.create(MODID);
+    public static final StaticModifier<ModifierRegister.Rude> rude = MODIFIERS.register("rude", ModifierRegister.Rude::new);
 
 
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().nutrition(20).saturationMod(20f).build())));
+    public static final RegistryObject<CreativeModeTab> mytab = CREATIVE_MODE_TAB.register("endtinkers_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("Endtinkers_tab"))
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> dot_apple.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(DOT_BLOCK_ITEM.get());
+                output.accept(dot_item.get());
+                output.accept(dot_apple.get());
+            }).build()
+    );
+
 
 
 
@@ -65,17 +84,18 @@ public class Endtinkers {
 
         ITEMS.register(modEventBus);
 
-        CREATIVE_MODE_TABS.register(modEventBus);
+        CREATIVE_MODE_TAB.register(modEventBus);
 
 
         MinecraftForge.EVENT_BUS.register(this);
 
-
-        modEventBus.addListener(this::addCreative);
+        Endtinkers.MODIFIERS.register(modEventBus);
 
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
+
+
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
@@ -90,9 +110,6 @@ public class Endtinkers {
     }
 
 
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) event.accept(EXAMPLE_BLOCK_ITEM);
-    }
 
 
     @SubscribeEvent
---
+++ src/main/java/com/endtinkers/modifier/register/ModifierRegister.java
@@ -0,0 +1,44 @@
+package com.endtinkers.modifier.register;
+
+
+import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.NotNull;
+import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.combat.MeleeDamageModifierHook;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
+import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+
+public class ModifierRegister {
+
+
+
+    public static class Rude extends Modifier implements MeleeDamageModifierHook {
+        public Rude() {
+        }
+
+
+        @Override
+        protected void registerHooks(ModuleHookMap.@NotNull Builder hookBuilder) {
+
+            hookBuilder.addHook(this, ModifierHooks.MELEE_DAMAGE);
+        }
+
+        //MeleeDamage方法修改工具伤害
+        @Override
+        public float getMeleeDamage(IToolStackView tool, ModifierEntry modifier, ToolAttackContext context, float baseDamage, float damage) {
+            //将实体形参context.getLivingTarget()定义为名为enemy的一个LivingEntity
+            LivingEntity enemy = context.getLivingTarget();
+            //检测该实体最大生命值是否大于20点
+            if (enemy != null && enemy.getMaxHealth() > 20) {
+                //是，则输出原伤害+1000
+                return damage + 1000;
+            }
+            //否则输出这个原伤害
+            return damage;
+        }
+
+    }
+}
